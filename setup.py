519b6c8a428c0b28a6eab58a8b83ed59cb75740a
Noemi Vanyi <sitbackandwait@gmail.com>
2015-02-16 00:11:16 +0100

    requirements: added

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from distutils.core import setup
 import wfw
 
 setup(
     name='wfw',
     packages=['wfw'],
-    version='0.1.2',
+    version='0.1.3',
     description='command-line interface for WorkFlowy',
     author='Noemi Vanyi',
     author_email='sitbackandwait@gmail.com',
     url='https://github.com/kvch/wfw',
-    download_url='https://github.com/kvch/wfw/tarball/0.1.2',
+    download_url='https://github.com/kvch/wfw/tarball/0.1.3',
     install_requires=[
-        'Click',
-        'Requests',
+        'click',
+        'requests',
     ],
     entry_points={
         'console_scripts' : [ 
