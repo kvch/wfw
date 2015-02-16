5bd7b34b352fcd3a38a0b323f000c7ba535cb8f8
Noemi Vanyi <sitbackandwait@gmail.com>
2015-02-16 22:57:06 +0100

    commands: break lib into two modules

--- wfw/lib.py
+++ wfw/lib.py
@@ -1,38 +1,11 @@
-import requests
 import json
 from os.path import expanduser
 from wfw.wfexceptions import LoginFailedException
+from wfw.api import log_in, log_out, get_list_from_server
 from wfw.tree import Tree
 
 
-WFURL = 'https://workflowy.com/'
 TREE_DATA = expanduser('~/.wfwtree')
-
-
-def log_in(email, password):
-    info = {'username' : email, 'password': password, 'next' : ''}
-    request = requests.post(WFURL + 'accounts/login/', data=info)
-
-    if not len(request.history) == 1:
-        raise LoginFailedException("Login was not successful")
-
-    cookies = requests.utils.dict_from_cookiejar(request.history[0].cookies)
-    return cookies['sessionid']
-
-
-def log_out(session_id):
-    cookie = {'sessionid' : session_id}
-    requests.get(WFURL + 'offline_logout', cookies=cookie)
-
-
-def get_list_from_server(session_id):
-    cookie = {'sessionid' : session_id}
-    request = requests.post(WFURL + 'get_initialization_data?client_version=14',
-                            cookies=cookie)
-
-    tree = request.json()
-    with open(TREE_DATA, 'w') as tree_data:
-        json.dump(tree, tree_data)
 
 
 def build_tree_from_file():
