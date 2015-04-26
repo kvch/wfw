e0205c525679db2232a9ac72d1bed399f3bd241e
Noemi Vanyi <sitbackandwait@gmail.com>
2015-04-26 22:47:36 +0200

    new version: 0.2

--- setup.py
+++ setup.py
@@ -5,24 +5,25 @@ import wfw
 setup(
     name='wfw',
     packages=['wfw'],
-    version='0.1.3',
+    version='0.2',
     description='command-line interface for WorkFlowy',
     author='Noemi Vanyi',
     author_email='sitbackandwait@gmail.com',
     url='https://github.com/kvch/wfw',
-    download_url='https://github.com/kvch/wfw/tarball/0.1.3',
+    download_url='https://github.com/kvch/wfw/tarball/0.2',
     license='GNU General Public License v3',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python'
+        'Programming Language :: Python :: 2.7'
     ],
+    long_description="WorkFlowy command-line tool for people who prefer the terminal.",
     install_requires=[
         'click',
         'requests',
     ],
     entry_points={
-        'console_scripts' : [ 
+        'console_scripts' : [
             'wfw = wfw.__main__:cli'
         ],
     })
