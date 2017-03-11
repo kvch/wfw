517993a7b2e6c1f8b8d582799ffb6e0fc782398e
Noémi Ványi <sitbackandwait@gmail.com>
2017-03-11 19:47:25 +0100

    tree: create class && add is_shared

--- wfw/lib.py
+++ wfw/lib.py
@@ -1,30 +1,21 @@
 from ConfigParser import RawConfigParser
 from os.path import expanduser, isfile
+import json
 
 from wfw.api import log_in, log_out, get_list_from_server, post_local_change
-from wfw.tree import (build,
-                      export_tree,
-                      find_nodes,
-                      find_tag,
-                      get_agenda,
-                      get_node,
-                      get_node_info,
-                      print_agenda,
-                      print_by_name,
-                      print_by_node,
-                      print_node_list,
-                      print_stats)
+from wfw.tree import Tree
 from wfw.wfexceptions import LocalChangePostingError, NodeNotFoundError
 
 
 TREE_DATA = expanduser('~/.wfwtree')
 USER_DATA = expanduser('~/.wfwrc')
-ROOT = {'id' : "None", 'text' : 'My list', 'children' : [], 'done' : False}
 
 
-def build_tree_from_file():
+def get_tree_from_file():
+    tree = None
     with open(TREE_DATA, 'r') as tree_data:
-        build(tree_data, ROOT)
+        tree = Tree(json.load(tree_data))
+    return tree
 
 
 def get_user_data():
@@ -35,6 +26,7 @@ def get_user_data():
             configparser.readfp(config)
         user_data['email'] = configparser.get('user', 'email')
         user_data['password'] = configparser.get('user', 'password')
+        user_data['aux'] = configparser.get('tree', 'shared')
         return user_data
 
 
@@ -50,36 +42,36 @@ def fetch_list():
 
 
 def print_list(depth, selected_root=None):
-    build_tree_from_file()
+    tree = get_tree_from_file()
     try:
         if selected_root is None:
-            print_by_node(ROOT, depth)
+            tree.print_by_node(depth)
         else:
-            print_by_name(selected_root, depth, ROOT)
+            tree.print_by_name(selected_root, depth)
     except NodeNotFoundError:
         raise
 
 
 def export_list(file_name):
-    build_tree_from_file()
-    export_tree(file_name, ROOT)
+    tree = get_tree_from_file()
+    tree.export(file_name)
 
 
 def search_nodes(pattern):
-    build_tree_from_file()
-    result = find_nodes(ROOT, pattern)
-    print_node_list(result)
+    tree = get_tree_from_file()
+    result = tree.find_nodes(tree.root, pattern)
+    tree.print_node_list(result)
 
 
 def search_tags(tag):
-    build_tree_from_file()
-    result = find_tag(ROOT, tag)
-    print_node_list(result)
+    tree = get_tree_from_file()
+    result = tree.find_tag(tree.root, tag)
+    tree.print_node_list(result)
 
 
 def add_item(parent_item, new_item):
-    build_tree_from_file()
-    parent_node = get_node(ROOT, parent_item)
+    tree = get_tree_from_file()
+    parent_node = tree.get_node(self.root, parent_item)
     user = get_user_data()
     try:
         session_id = log_in(user['email'], user['password'])
@@ -95,9 +87,9 @@ def add_item(parent_item, new_item):
 
 
 def remove_item(parent_item, deleted_item):
-    build_tree_from_file()
-    parent_node = get_node(ROOT, parent_item)
-    deleted_node = get_node(ROOT, deleted_item)
+    tree = get_tree_from_file()
+    parent_node = tree.get_node(tree.root, parent_item)
+    deleted_node = tree.get_node(tree.root, deleted_item)
     user = get_user_data()
     try:
         session_id = log_in(user['email'], user['password'])
@@ -113,21 +105,21 @@ def remove_item(parent_item, deleted_item):
 
 
 def calc_item_stats(item):
-    build_tree_from_file()
-    node = get_node(ROOT, item)
-    number_children, done = get_node_info(node)
+    tree = get_tree_from_file()
+    node = tree.get_node(tree.root, item)
+    number_children, done = tree.get_node_info(node)
     if node['done']:
         done = done - 1
     if number_children > 0:
         progress = round(done / float(number_children) * 100, 2)
     else:
         progress = 0
-    print_node_list([node])
-    print_stats(number_children, done, progress)
+    tree.print_node_list([node])
+    tree.print_stats(number_children, done, progress)
 
 def show_agenda(item):
-    build_tree_from_file()
-    node = get_node(ROOT, item)
+    tree = get_tree_from_file()
+    node = tree.get_node(tree.root, item)
     if node:
-        events = get_agenda(node)
-        print_agenda(events)
+        events = tree.get_agenda(node)
+        tree.print_agenda(events)
--- wfw/tree.py
+++ wfw/tree.py
@@ -12,161 +12,144 @@ WHITE = '\033[37m'
 END = '\033[0m'
 
 
-def add_node(node, parent):
-    is_done = 'cp' in node.keys()
-    new_node = {'id' : node['id'],
-                'text' : node['nm'].encode('utf-8'),
-                'children' : [],
-                'done' : is_done, 'parent' : parent['text']}
-    parent['children'].append(new_node)
+class Tree(object):
+    def __init__(self, raw):
+        self.root = {'id' : "None", 'text' : 'My list', 'children' : [], 'done' : False}
+        children_of_root = raw['projectTreeData']['mainProjectTreeInfo']['rootProjectChildren']
 
-    if 'ch' in node.keys():
-        for child in node['ch']:
-            add_node(child, new_node)
+        for child in children_of_root:
+            self._add_node(child, self.root)
 
+    def _add_node(self, node, parent):
+        is_done = 'cp' in node.keys()
+        new_node = {'id' : node['id'],
+                    'text' : node['nm'].encode('utf-8'),
+                    'children' : [],
+                    'done' : is_done, 'parent' : parent['text']}
+        parent['children'].append(new_node)
 
-def build(input_file, root):
-    raw_tree = json.load(input_file)
-    children_of_root = raw_tree['projectTreeData']['mainProjectTreeInfo']['rootProjectChildren']
+        if 'ch' in node.keys():
+            for child in node['ch']:
+                self._add_node(child, new_node)
 
-    for child in children_of_root:
-        add_node(child, root)
-
-
-def get_node(node, text_to_find):
-    if node['text'] == text_to_find.encode('utf-8'):
-        return node
-
-    for child in node['children']:
-        result = get_node(child, text_to_find)
-        if result:
-            return result
-
-
-def find_nodes(node, text_to_find, result=[]):
-    pattern = re.compile(text_to_find)
-    for child in node['children']:
-        if pattern.match(child['text']):
-            result.append(child)
-        find_nodes(child, text_to_find, result)
-
-    return result
-
-
-def find_tag(node, tag, result=[]):
-    if tag[0] == '#' or tag[0] == '@':
+    def get_node(self, node, text_to_find):
+        if node['text'] == text_to_find.encode('utf-8'):
+            return node
         for child in node['children']:
-            if tag.encode('utf-8') in child['text']:
+            result = self.get_node(child, text_to_find)
+            if result:
+                return result
+
+    def find_nodes(self, node, text_to_find, result=[]):
+        pattern = re.compile(text_to_find)
+        for child in node['children']:
+            if pattern.match(child['text']):
                 result.append(child)
-            find_tag(child, tag, result)
+            self.find_nodes(child, text_to_find, result)
 
         return result
-    raise InvalidTagFormatException
 
+    def find_tag(self, node, tag, result=[]):
+        if tag[0] == '#' or tag[0] == '@':
+            for child in node['children']:
+                if tag.encode('utf-8') in child['text']:
+                    result.append(child)
+                self.find_tag(child, tag, result)
 
-def write_to_file(destination, start, depth=0):
-    destination.write(exportable_format(start, depth))
-    for child in start['children']:
-        write_to_file(destination, child, depth+1)
+            return result
+        raise InvalidTagFormatException
 
-
-def export_tree(file_name, root):
-    with open(file_name, 'w') as destination:
-        write_to_file(destination, root)
-
-
-def get_node_info(node, children_number=0, done=0):
-    children_number += len(node['children'])
-    if node['done']:
-        done += 1
-
-    for child in node['children']:
-        children_number, done = get_node_info(child, children_number, done)
-
-    return (children_number, done)
-
-
-def print_by_node(start, depth, current_depth=0):
-    print(printable_format(start, current_depth))
-
-    if depth > current_depth:
-        current_depth += 1
+    def _write_to_file(self, destination, start, depth=0):
+        destination.write(exportable_format(start, depth))
         for child in start['children']:
-            print_by_node(child, depth, current_depth)
+            self.write_to_file(destination, child, depth+1)
 
+    def export_tree(self, file_name):
+        with open(file_name, 'w') as destination:
+            self.write_to_file(destination, self.root)
 
-def print_by_name(name, depth, root):
-    selected_root = get_node(root, name)
+    def get_node_info(self, node, children_number=0, done=0):
+        children_number += len(node['children'])
+        if node['done']:
+            done += 1
+        for child in node['children']:
+            children_number, done = self.get_node_info(child, children_number, done)
 
-    if selected_root is None:
-        raise NodeNotFoundError
+        return (children_number, done)
 
-    print_by_node(selected_root, depth)
+    def print_by_node(self, start, depth, current_depth=0):
+        print(self._printable_format(start, current_depth))
 
+        if depth > current_depth:
+            current_depth += 1
+            for child in start['children']:
+                self.print_by_node(child, depth, current_depth)
 
-def print_node_list(nodelist):
-    for node in nodelist:
-        print("{}\n    parent: {}".format(printable_format(node), node['parent']))
+    def print_by_name(self, name, depth):
+        selected_root = self.get_node(self.root, name)
+        if selected_root is None:
+            raise NodeNotFoundError
+        self.print_by_node(selected_root, depth)
 
+    def print_node_list(self, nodelist):
+        for node in nodelist:
+            if 'parent' in node:
+                print("{}\n    parent: {}".format(self._printable_format(node), node['parent']))
 
-def exportable_format(node, depth):
-    return "{fill}{name}\n".format(fill='\t' * depth, name=node['text'])
+    def _exportable_format(self, node, depth):
+        return "{fill}{name}\n".format(fill='\t' * depth, name=node['text'])
 
+    def _printable_format(self, node, depth=0):
+        name = node['text']
 
-def printable_format(node, depth=0):
-    name = node['text']
+        if node['done']:
+            name = "{}{}{}".format(DIM, name, END)
+        elif '<b>' in name and '</b>' in name:
+            name = name.replace('<b>', BRIGHT)
+            name = name.replace('</b>', END)
 
-    if node['done']:
-        name = "{}{}{}".format(DIM, name, END)
-    elif '<b>' in name and '</b>' in name:
-        name = name.replace('<b>', BRIGHT)
-        name = name.replace('</b>', END)
+        for tag in ('@', '#'):
+            if tag in name:
+                index = name.index(tag)
+                name = "{}{}{}".format(name[:index], YELLOW, name[index:])
+                if not name.endswith(END):
+                    name = name + END
 
-    for tag in ('@', '#'):
-        if tag in name:
-            index = name.index(tag)
-            name = "{}{}{}".format(name[:index], YELLOW, name[index:])
-            if not name.endswith(END):
-                name = name + END
+        return "{fill}* {name}".format(fill='    ' * depth, name=name)
 
-    return "{fill}* {name}".format(fill='    ' * depth, name=name)
+    def print_stats(self, children, done, progress):
+        print("    subtasks: {}".format(children))
+        print("    done: {}".format(done))
+        print("    progress: {}%".format(progress))
 
+    def get_agenda(self, node):
+        events = []
+        for child in node['children']:
+            event = dict()
+            event_words = []
+            tokens = child['text'].split(' ')
+            for token in tokens:
+                if token.startswith('@'):
+                    event['place'] = token[1:]
+                elif token.startswith('#'):
+                    event['date'] = token[1:]
+                else:
+                    event_words.append(token)
+            event['desc'] = " ".join(event_words)
+            if 'date' in event:
+                events.append(event)
 
-def print_stats(children, done, progress):
-    print("    subtasks: {}".format(children))
-    print("    done: {}".format(done))
-    print("    progress: {}%".format(progress))
+        events.sort(key=itemgetter('date'))
 
+        return events
 
-def get_agenda(node):
-    events = []
-    for child in node['children']:
-        event = dict()
-        event_words = []
-        tokens = child['text'].split(' ')
-        for token in tokens:
-            if token.startswith('@'):
-                event['place'] = token[1:]
-            elif token.startswith('#'):
-                event['date'] = token[1:]
-            else:
-                event_words.append(token)
-        event['desc'] = " ".join(event_words)
-        if 'date' in event:
-            events.append(event)
-
-    events.sort(key=itemgetter('date'))
-
-    return events
-
-
-def print_agenda(events):
-    current_date = events[0]['date']
-    print("---{}---".format(current_date))
-    for event in events:
-        if not current_date == event['date']:
-            current_date = event['date']
-            print("---{}---".format(current_date))
-        print(" {}".format(event['desc']))
-        if "place" in event:
-            print("    // {}".format(event['place']))
+    def print_agenda(self, events):
+        current_date = events[0]['date']
+        print("---{}---".format(current_date))
+        for event in events:
+            if not current_date == event['date']:
+                current_date = event['date']
+                print("---{}---".format(current_date))
+            print(" {}".format(event['desc']))
+            if "place" in event:
+                print("    // {}".format(event['place']))
